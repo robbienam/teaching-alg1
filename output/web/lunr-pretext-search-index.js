81bb19c2f53458a56939e2ff07b580f7635c9981
Robbie McNerney <robbienam@gmail.com>
2025-08-16 18:35:21 -0400

    updating blank origin repo with local pretext project

---
+++ output/web/lunr-pretext-search-index.js
@@ -0,0 +1,149 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "section-numerical-expressions",
+  "level": "1",
+  "url": "section-numerical-expressions.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Numerical Expressions",
+  "body": " Numerical Expressions    This section covers the fundamentals of writing, evaluating, and applying numerical expressions. We will review how to translate verbal phrases into mathematical expressions and the importance of the order of operations.     Learn : Writing Numerical Expressions   Translate a Verbal Expression   The verbal expression \"twelve divided by four plus two\" can be translated into the numerical expression:     .     Translate a Verbal Expression   Translate the verbal expression \"eight minus three times two\" into a numerical expression.    The numerical expression is .     Translate a Verbal Expression with Grouping Symbols   Consider the verbal expression \"two times the difference of 6 and 4\". This can be written as .    Translate a Verbal Expression with Grouping Symbols   Translate the verbal expression \"twelve divided by the sum of four and two\" into a numerical expression.    The numerical expression is .     Write a Numerical Expression   The verbal expression \"the difference of nine and five times the sum of three and two\" can be written as .    Write a Numerical Expression   Write the numerical expression for the verbal phrase \"the sum of eight and seven divided by the difference of four and one\".    The numerical expression is .       Learn : Evaluating Numerical Expressions   In this subsection, we will learn how to evaluate numerical expressions using the order of operations.   Order of Operations  To evaluate an expression, we must follow the correct order of operations . These rules can be remembered by their acronym PEMDAS:   Parentheses    Exponents    Multiplication and Division (from left to right)    Addition and Subtraction (from left to right)     Note: Multiplication does not always come before Division, and Addition does not always come before Subtraction. They are performed from left to right as they appear in the expression.   For example, when evaluating the expression we follow the order of operations as shown below: First, we evaluate inside the parentheses. Next, we compute the exponent. Then, we multiply. Finally, we add and get the expression's value of .    Order of Operations Exercise   Evaluate the expression using the order of operations.    The result is .     Evaluate a Numerical Expression   To evaluate the expression , we first calculate the sum inside the parentheses, which gives us , and then multiply by to get .    Write and Evaluate a Numerical Expression   For the verbal expression \"the difference of 10 and 4 multiplied by 2\", we write it as and evaluate it to get .    Evaluate a Numerical Expression with Grouping Symbols   For the expression , we first evaluate the expressions in parentheses to get and , respectively, and then multiply them to get .      Learn : Plan for Problem Solving   Write and Evaluate Expressions   When faced with a problem, first write down the expression that represents the problem, then evaluate it step by step.      Exit Ticket        Practice and HW      "
+},
+{
+  "id": "example-translate-verbal-expression",
+  "level": "2",
+  "url": "section-numerical-expressions.html#example-translate-verbal-expression",
+  "type": "Example",
+  "number": "1.1.1",
+  "title": "Translate a Verbal Expression.",
+  "body": " Translate a Verbal Expression   The verbal expression \"twelve divided by four plus two\" can be translated into the numerical expression:     .   "
+},
+{
+  "id": "exercise-translate-verbal-expression",
+  "level": "2",
+  "url": "section-numerical-expressions.html#exercise-translate-verbal-expression",
+  "type": "Checkpoint",
+  "number": "1.1.2",
+  "title": "Translate a Verbal Expression.",
+  "body": " Translate a Verbal Expression   Translate the verbal expression \"eight minus three times two\" into a numerical expression.    The numerical expression is .   "
+},
+{
+  "id": "example-translate-verbal-expression-with-grouping",
+  "level": "2",
+  "url": "section-numerical-expressions.html#example-translate-verbal-expression-with-grouping",
+  "type": "Example",
+  "number": "1.1.3",
+  "title": "Translate a Verbal Expression with Grouping Symbols.",
+  "body": " Translate a Verbal Expression with Grouping Symbols   Consider the verbal expression \"two times the difference of 6 and 4\". This can be written as .  "
+},
+{
+  "id": "exercise-translate-verbal-expression-with-grouping",
+  "level": "2",
+  "url": "section-numerical-expressions.html#exercise-translate-verbal-expression-with-grouping",
+  "type": "Checkpoint",
+  "number": "1.1.4",
+  "title": "Translate a Verbal Expression with Grouping Symbols.",
+  "body": " Translate a Verbal Expression with Grouping Symbols   Translate the verbal expression \"twelve divided by the sum of four and two\" into a numerical expression.    The numerical expression is .   "
+},
+{
+  "id": "example-write-numerical-expression",
+  "level": "2",
+  "url": "section-numerical-expressions.html#example-write-numerical-expression",
+  "type": "Example",
+  "number": "1.1.5",
+  "title": "Write a Numerical Expression.",
+  "body": " Write a Numerical Expression   The verbal expression \"the difference of nine and five times the sum of three and two\" can be written as .  "
+},
+{
+  "id": "exercise-write-numerical-expression",
+  "level": "2",
+  "url": "section-numerical-expressions.html#exercise-write-numerical-expression",
+  "type": "Checkpoint",
+  "number": "1.1.6",
+  "title": "Write a Numerical Expression.",
+  "body": " Write a Numerical Expression   Write the numerical expression for the verbal phrase \"the sum of eight and seven divided by the difference of four and one\".    The numerical expression is .   "
+},
+{
+  "id": "example-order-of-operations",
+  "level": "2",
+  "url": "section-numerical-expressions.html#example-order-of-operations",
+  "type": "Example",
+  "number": "1.1.7",
+  "title": "Order of Operations.",
+  "body": " Order of Operations  To evaluate an expression, we must follow the correct order of operations . These rules can be remembered by their acronym PEMDAS:   Parentheses    Exponents    Multiplication and Division (from left to right)    Addition and Subtraction (from left to right)     Note: Multiplication does not always come before Division, and Addition does not always come before Subtraction. They are performed from left to right as they appear in the expression.   For example, when evaluating the expression we follow the order of operations as shown below: First, we evaluate inside the parentheses. Next, we compute the exponent. Then, we multiply. Finally, we add and get the expression's value of .  "
+},
+{
+  "id": "exercise-order-of-operations",
+  "level": "2",
+  "url": "section-numerical-expressions.html#exercise-order-of-operations",
+  "type": "Checkpoint",
+  "number": "1.1.8",
+  "title": "Order of Operations Exercise.",
+  "body": " Order of Operations Exercise   Evaluate the expression using the order of operations.    The result is .   "
+},
+{
+  "id": "example-evaluate-numerical-expression",
+  "level": "2",
+  "url": "section-numerical-expressions.html#example-evaluate-numerical-expression",
+  "type": "Example",
+  "number": "1.1.9",
+  "title": "Evaluate a Numerical Expression.",
+  "body": " Evaluate a Numerical Expression   To evaluate the expression , we first calculate the sum inside the parentheses, which gives us , and then multiply by to get .  "
+},
+{
+  "id": "exanmple-write-and-evaluate-numerical-expression",
+  "level": "2",
+  "url": "section-numerical-expressions.html#exanmple-write-and-evaluate-numerical-expression",
+  "type": "Example",
+  "number": "1.1.10",
+  "title": "Write and Evaluate a Numerical Expression.",
+  "body": " Write and Evaluate a Numerical Expression   For the verbal expression \"the difference of 10 and 4 multiplied by 2\", we write it as and evaluate it to get .  "
+},
+{
+  "id": "example-evaluate-numerical-expression-with-grouping",
+  "level": "2",
+  "url": "section-numerical-expressions.html#example-evaluate-numerical-expression-with-grouping",
+  "type": "Example",
+  "number": "1.1.11",
+  "title": "Evaluate a Numerical Expression with Grouping Symbols.",
+  "body": " Evaluate a Numerical Expression with Grouping Symbols   For the expression , we first evaluate the expressions in parentheses to get and , respectively, and then multiply them to get .  "
+},
+{
+  "id": "example-write-and-evaluate-expressions",
+  "level": "2",
+  "url": "section-numerical-expressions.html#example-write-and-evaluate-expressions",
+  "type": "Example",
+  "number": "1.1.12",
+  "title": "Write and Evaluate Expressions.",
+  "body": " Write and Evaluate Expressions   When faced with a problem, first write down the expression that represents the problem, then evaluate it step by step.  "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
