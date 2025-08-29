8af0fd25fa53f19e446f89a62ae523b812bc7698
Robbie McNerney <robbienam@gmail.com>
2025-08-28 22:31:28 -0400

    added section Line Segments

--- output/web/lunr-pretext-search-index.js
+++ output/web/lunr-pretext-search-index.js
@@ -10,36 +10,36 @@ var ptx_lunr_docs = [
   "body": "  "
 },
 {
-  "id": "sec-numerical-expressions_2",
+  "id": "sec-numerical-expressions",
   "level": "1",
-  "url": "sec-numerical-expressions_2.html",
+  "url": "sec-numerical-expressions.html",
   "type": "Section",
   "number": "1.1",
   "title": "Numerical Expressions",
   "body": " Numerical Expressions     Learning Objectives    Write numerical expressions from verbal expressions.    Evaluate numerical expressions using the order of operations.    Apply numerical expressions to solve real-world problems.      Language Objectives     Use mathematical language to describe numerical expressions and their evaluation.    Explain the order of operations and its importance in evaluating numerical expressions.    Communicate the process of writing and evaluating numerical expressions in real-world contexts.           Background you need for this lesson:   Adding, subtracting, multiplying, and dividing fractions. Review Fraction Arithmetic     Simplifying a fraction to lowest terms: Practice Simplifying Fractions (Colab)       This section covers the fundamentals of writing, evaluating, and applying numerical expressions.  Suppose your kitchen sink has sprung a leak. You hire a plumber who charges a flat $50 service fee plus $75 per hour of labor. If the plumber works for 3 hours, how much will you owe?  To calculate the total cost, you could write the following mathematical expression: Evaluating this expression gives the total cost of $275.  The above expression is an example of a numerical expression .    A numerical expression is an expression formed only from numbers and the basic operations of arithmetic. Numerical expressions may also contain grouping symbols like: .     The following are additional examples of numerical expressions:    In this section, we will learn how to write numerical expressions from their verbal descriptions, evaluate them using the order of operations, and apply them to solve real-world problems.     Learn : Writing Numerical Expressions   Translate a Verbal Expression without Grouping Symbols  The verbal expression \"twelve divided by four plus two\" can be translated into the numerical expression:   In general, we translate simple verbal expressions into numerical expressions as follows:    \"five plus three\" (Addition)    \"five minus three\" (Subtraction)    \"five times three\" (Multiplication)    \"five divided by three\" or (Division)      Translate a Verbal Expression   Translate the verbal expression \"eight minus three times two\" into a numerical expression.     .      Translate a Verbal Expression with Grouping Symbols  Some verbal expressions contain special phrases that refer to the result of a basic arithmetic operations. Such expressions require the use of grouping symbols to properly translate them into numerical expressions.  For example, the verbal expression \" the sum of five and three times two\" is translated into the numerical expression:   Note in above how we have enclosed the addition operation in parentheses: \"the sum of five and three\" becomes . This is necessary, because when we speak of the \"sum\" of two numbers, we want to compute with the output of that operation rather than any of its internal parts.  In general, we translate verbal expressions that require grouping symbols as follows:   \"the sum of\" refers to the result of addition     \"the difference of\" refers to the result of subtraction     \"the product of\" refers to the result of multiplication     \"the quotient of\" refers to the result of division       Translate a Verbal Expression with Grouping Symbols    Write a numerical expression for each of the following verbal expressions:   \"two times the difference of six and four\".    \"twelve divided by the sum of four and two\".    \"the sum of eight and seven divided by the difference of four and one\".    \"the difference of nine and five times the sum of three and two\".           .     .     .     .           Learn : Evaluating Numerical Expressions   We have learned how to write numerical expressions from verbal descriptions. But what do such expressions actually mean?  Recall the plumbing problem from above. Suppose you were to hand your friend a slip of paper with just the above expression written on it. Without knowing anything about how you were using this expression, your friend should still be able to correctly interpret and evaluate it to get its value of 275.  That is the power of mathematical language: it allows us to communicate ideas precisely and unambiguously . Like all mathematical language, numerical expressions are powerful because they communicate a precise meaning independent from any particular problem they model.  For numerical expressions, the order of operations tells us the proper sequence for combining the numbers in the expression using the given operations. In this way, we can evaluate every numerical expression to a single numerical value.    The order of operations gives the proper order to evaluate any numerical expression:  First, evaluate inside Parentheses (...) and other grouping symbols. For nested grouping symbols, begin with the innermost groupings and work outwards.  Next, evaluate Exponents (powers and roots or radicals).  Then, perform Multiplication and Division from left to right.  Finally, perform Addition and Subtraction from left to right.  Note: Multiplication does not always come before Division, and Addition does not always come before Subtraction. They are performed from left to right as they appear in the expression.  The above order of operations can be remembered using the acronym PEMDAS , which stands for Parentheses, Exponents, Multiplication\/Division, Addition\/Subtraction.     Evaluate a numerical expression using the Order of Operations    For example, when evaluating the expression we follow the order of operations as shown below: First, we evaluate inside the parentheses. Next, we compute the exponent. Then, we multiply. Finally, we add and get the expression's value of .     Evaluate a Numerical Expression   Evaluate the expression .     .     Evaluate a Numerical Expression with Grouping Symbols    Evaluate the expression .     .     Evaluate a Numerical Expression with Nested Grouping Symbols    Evaluate the expression .     .     Evaluate numerical expressions using the order of operations   Use the order of operations to evaluate the following numerical expressions:             Learn : Plan for Problem Solving   To solve real-world problems, we often need to write numerical expressions that model the situation. The key steps are:  Identify the quantities involved.  Determine the operations needed to relate these quantities.  Write a numerical expression that captures the relationships.  Evaluate the expression to find the solution.     Write and Evaluate Expressions   Write a numerical expression for the following problem: \"A store sells apples for $2 each and oranges for $3 each. If you buy 4 apples and 5 oranges, how much do you spend in total?\"    The numerical expression is , which evaluates to $23.       Exit Ticket    Reflect on what you have learned in this section by completing the following tasks:   Write a numerical expression that represents some real-world quantity in your life (e.g., think about your time, money, school, job, etc.).    Turning to a neighbor, explain your expression and how it correctly models the quantity you have identified in your life. Then, describe how to evaluate it using the order of operations to find its value.       Key Terms   Numerical Expression  A mathematical expression that contains only numbers and arithmetic operations (no variables).    Order of Operations  The rules that define the correct order to perform the arithmetic operations of a numerical expression.    Evaluate  To calculate the number value of a numerical expression by performing its operations in the correct order.    Grouping Symbols  Symbols such as parentheses , brackets , and braces that indicate which operations should be performed first in a numerical expression.       Practice and HW      "
 },
 {
-  "id": "sec-numerical-expressions_2-2",
+  "id": "sec-numerical-expressions-2",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#sec-numerical-expressions_2-2",
+  "url": "sec-numerical-expressions.html#sec-numerical-expressions-2",
   "type": "Objectives",
   "number": "1.1",
   "title": "",
   "body": "   Learning Objectives    Write numerical expressions from verbal expressions.    Evaluate numerical expressions using the order of operations.    Apply numerical expressions to solve real-world problems.      Language Objectives     Use mathematical language to describe numerical expressions and their evaluation.    Explain the order of operations and its importance in evaluating numerical expressions.    Communicate the process of writing and evaluating numerical expressions in real-world contexts.       "
 },
 {
-  "id": "sec-numerical-expressions_2-3-1",
+  "id": "sec-numerical-expressions-3-1",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#sec-numerical-expressions_2-3-1",
+  "url": "sec-numerical-expressions.html#sec-numerical-expressions-3-1",
   "type": "Note",
   "number": "1.1.1",
   "title": "",
   "body": "  Background you need for this lesson:   Adding, subtracting, multiplying, and dividing fractions. Review Fraction Arithmetic     Simplifying a fraction to lowest terms: Practice Simplifying Fractions (Colab)      "
 },
 {
-  "id": "sec-numerical-expressions_2-3-5",
+  "id": "sec-numerical-expressions-3-5",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#sec-numerical-expressions_2-3-5",
+  "url": "sec-numerical-expressions.html#sec-numerical-expressions-3-5",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
@@ -48,7 +48,7 @@ var ptx_lunr_docs = [
 {
   "id": "def-numerical-expression",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#def-numerical-expression",
+  "url": "sec-numerical-expressions.html#def-numerical-expression",
   "type": "Definition",
   "number": "1.1.2",
   "title": "",
@@ -57,7 +57,7 @@ var ptx_lunr_docs = [
 {
   "id": "example-numerical-expressions",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#example-numerical-expressions",
+  "url": "sec-numerical-expressions.html#example-numerical-expressions",
   "type": "Example",
   "number": "1.1.3",
   "title": "",
@@ -66,7 +66,7 @@ var ptx_lunr_docs = [
 {
   "id": "exercise-translate-verbal-expression-without-grouping",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#exercise-translate-verbal-expression-without-grouping",
+  "url": "sec-numerical-expressions.html#exercise-translate-verbal-expression-without-grouping",
   "type": "Checkpoint",
   "number": "1.1.4",
   "title": "Translate a Verbal Expression.",
@@ -75,7 +75,7 @@ var ptx_lunr_docs = [
 {
   "id": "example-translate-verbal-expression-with-grouping",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#example-translate-verbal-expression-with-grouping",
+  "url": "sec-numerical-expressions.html#example-translate-verbal-expression-with-grouping",
   "type": "Example",
   "number": "1.1.5",
   "title": "Translate a Verbal Expression with Grouping Symbols.",
@@ -84,7 +84,7 @@ var ptx_lunr_docs = [
 {
   "id": "subsection-evaluating-numerical-expressions-5",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#subsection-evaluating-numerical-expressions-5",
+  "url": "sec-numerical-expressions.html#subsection-evaluating-numerical-expressions-5",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
@@ -93,7 +93,7 @@ var ptx_lunr_docs = [
 {
   "id": "def-order-of-operations",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#def-order-of-operations",
+  "url": "sec-numerical-expressions.html#def-order-of-operations",
   "type": "Definition",
   "number": "1.1.6",
   "title": "",
@@ -102,7 +102,7 @@ var ptx_lunr_docs = [
 {
   "id": "example-evaluate-numerical-expression",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#example-evaluate-numerical-expression",
+  "url": "sec-numerical-expressions.html#example-evaluate-numerical-expression",
   "type": "Example",
   "number": "1.1.7",
   "title": "Evaluate a numerical expression using the Order of Operations.",
@@ -111,7 +111,7 @@ var ptx_lunr_docs = [
 {
   "id": "example-evaluate-numerical-expression-with-fraction",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#example-evaluate-numerical-expression-with-fraction",
+  "url": "sec-numerical-expressions.html#example-evaluate-numerical-expression-with-fraction",
   "type": "Example",
   "number": "1.1.8",
   "title": "Evaluate a Numerical Expression.",
@@ -120,7 +120,7 @@ var ptx_lunr_docs = [
 {
   "id": "example-evaluate-numerical-expression-with-grouping",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#example-evaluate-numerical-expression-with-grouping",
+  "url": "sec-numerical-expressions.html#example-evaluate-numerical-expression-with-grouping",
   "type": "Example",
   "number": "1.1.9",
   "title": "Evaluate a Numerical Expression with Grouping Symbols.",
@@ -129,7 +129,7 @@ var ptx_lunr_docs = [
 {
   "id": "example-evaluate-numerical-expression-with-nested-grouping-symbols",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#example-evaluate-numerical-expression-with-nested-grouping-symbols",
+  "url": "sec-numerical-expressions.html#example-evaluate-numerical-expression-with-nested-grouping-symbols",
   "type": "Example",
   "number": "1.1.10",
   "title": "Evaluate a Numerical Expression with Nested Grouping Symbols.",
@@ -138,7 +138,7 @@ var ptx_lunr_docs = [
 {
   "id": "exercise-evaluate-numerical-expressions",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#exercise-evaluate-numerical-expressions",
+  "url": "sec-numerical-expressions.html#exercise-evaluate-numerical-expressions",
   "type": "Checkpoint",
   "number": "1.1.11",
   "title": "Evaluate numerical expressions using the order of operations.",
@@ -147,7 +147,7 @@ var ptx_lunr_docs = [
 {
   "id": "exercise-write-and-evaluate-expressions",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#exercise-write-and-evaluate-expressions",
+  "url": "sec-numerical-expressions.html#exercise-write-and-evaluate-expressions",
   "type": "Checkpoint",
   "number": "1.1.12",
   "title": "Write and Evaluate Expressions.",
@@ -156,11 +156,83 @@ var ptx_lunr_docs = [
 {
   "id": "subsection-exit-ticket-2",
   "level": "2",
-  "url": "sec-numerical-expressions_2.html#subsection-exit-ticket-2",
+  "url": "sec-numerical-expressions.html#subsection-exit-ticket-2",
   "type": "Activity",
   "number": "1.1.1",
   "title": "",
   "body": "  Reflect on what you have learned in this section by completing the following tasks:   Write a numerical expression that represents some real-world quantity in your life (e.g., think about your time, money, school, job, etc.).    Turning to a neighbor, explain your expression and how it correctly models the quantity you have identified in your life. Then, describe how to evaluate it using the order of operations to find its value.     "
+},
+{
+  "id": "sec-line-segments",
+  "level": "1",
+  "url": "sec-line-segments.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Line Segments",
+  "body": " Line Segments     Learning Objectives    Calculate measurements of line segments.    Apply the definition of congruent line segments to find missing values.    Apply to solve real-world measurement problems.      Language Objectives     Describe the relation of betweenness among three points on a line.    Describe the relation of congruence between two line segments.         Line Segments    A line segment is a portion of a line consisting of two endpoints and all points on the line which lie between them. We label the line segment between points A and B as or equivalently , where A and B are the endpoints of the segment.    The length or measure of a line segment is the distance between its two endpoints. We denote the length of line segment as .  We now formally define the notion of betweenness of points .    Betweenness of Points    Given three collinear points A, B, and C, we say C is between A and B if AC + CB = AB.    Note, the notion of betweenness is defined only for three points which all lie on the same line. Betweenness is a property of points on a line.  For such triples of points on a line, the sum of the measure of the partial segments equals the measure of the whole segment. Therefore, we can use the concept of betweenness to find the lengths of line segments when given some of the lengths.  Example 1 Find Measurements by Adding  Example 2 Find Measurements by Subtracting  Example 3 Find Measurements by Writing and Solving Equations  Example 4 Use Betweenness of Points (Space Needle height)    Line Segment Congruence    Two geometric figures that have the same shape and size are said to be congruent .    Two line segments are congruent segments if they have the same measure or length.  We denote that line segments and are congruent by writing . This implies that the lengths of the segments are equal, i.e., .  Note well the following distinction: Two line segments can be congruent . Two lengths can be equal .  Example 5 Write and Solve Equations by Using Congruence   "
+},
+{
+  "id": "sec-line-segments-2",
+  "level": "2",
+  "url": "sec-line-segments.html#sec-line-segments-2",
+  "type": "Objectives",
+  "number": "1.2",
+  "title": "",
+  "body": "   Learning Objectives    Calculate measurements of line segments.    Apply the definition of congruent line segments to find missing values.    Apply to solve real-world measurement problems.      Language Objectives     Describe the relation of betweenness among three points on a line.    Describe the relation of congruence between two line segments.       "
+},
+{
+  "id": "def-line-segment",
+  "level": "2",
+  "url": "sec-line-segments.html#def-line-segment",
+  "type": "Definition",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  A line segment is a portion of a line consisting of two endpoints and all points on the line which lie between them. We label the line segment between points A and B as or equivalently , where A and B are the endpoints of the segment.   "
+},
+{
+  "id": "subsection-line-segments-3",
+  "level": "2",
+  "url": "sec-line-segments.html#subsection-line-segments-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "length measure "
+},
+{
+  "id": "subsection-line-segments-4",
+  "level": "2",
+  "url": "sec-line-segments.html#subsection-line-segments-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "betweenness of points "
+},
+{
+  "id": "def-betweenness-of-points",
+  "level": "2",
+  "url": "sec-line-segments.html#def-betweenness-of-points",
+  "type": "Definition",
+  "number": "1.2.2",
+  "title": "",
+  "body": "  Given three collinear points A, B, and C, we say C is between A and B if AC + CB = AB.   "
+},
+{
+  "id": "definition-congruence",
+  "level": "2",
+  "url": "sec-line-segments.html#definition-congruence",
+  "type": "Definition",
+  "number": "1.2.3",
+  "title": "",
+  "body": "  Two geometric figures that have the same shape and size are said to be congruent .   "
+},
+{
+  "id": "subsection-line-segment-congruence-3",
+  "level": "2",
+  "url": "sec-line-segments.html#subsection-line-segment-congruence-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "congruent segments "
 },
 {
   "id": "backmatter-2",
